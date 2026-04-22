018da9cdf16edb072f6289f7450e3b4ee65133a1
QualityLemons <parmanjohn@gmail.com>
2026-04-22 19:59:45 +0100

    feat: add urls.py for account-related routes

---
+++ accounts/views.py
@@ -0,0 +1,16 @@
+from django.urls import reverse_lazy
+from django.views.generic.edit import CreateView
+from django.contrib.auth.views import LoginView, LogoutView
+from .forms import CustomUserCreationForm
+
+class SignUpView(CreateView):
+    form_class = CustomUserCreationForm
+    success_url = reverse_lazy('login')
+    template_name = 'registration/signup.html'
+
+class UserLoginView(LoginView):
+    template_name = 'registration/login.html'
+    redirect_authenticated_user = True
+
+class UserLogoutView(LogoutView):
+    next_page = reverse_lazy('login')
